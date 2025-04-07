f1ebc4e0a41ca7b467b54a78d937f79ffaf21848
Branislav Macosinec <branomps@gmail.com>
2025-04-07 11:00:42 +0100

    Create Mercedez class with basic methods.

---
+++ mercedezbenz.py
@@ -0,0 +1,16 @@
+class MercedezBenz:
+    doors=4
+    model='G'
+    wheels=4
+
+    def __init__(self,color):
+        self.color = color
+
+    def drive(self):
+        return f"A Mercedez is driving. It is {self}\n"
+    
+    def auto_drive():
+        return "Auto is driving"
+    
+m1 = MercedezBenz("red")
+print(f"The Mercedez is: {m1}")
