e1594d68ddb7c72d76334ba3d2e686486700387d
Branislav Macosinec <branomps@gmail.com>
2025-04-08 10:09:39 +0100

    Class and Static methods.

--- mercedezbenz.py
+++ mercedezbenz.py
@@ -9,8 +9,17 @@ class MercedezBenz:
     def drive(self):
         return f"A Mercedez is driving. It is {self}\n"
     
-    def auto_drive(self):
-        return "Auto is driving"
+    @staticmethod
+    def auto_drive():
+        return f"Auto is driving\n"
+    
+    @classmethod
+    def create_lease(cls):
+        print(f"A lease for {cls} will be created")
     
 m1 = MercedezBenz("red")
 print(f"The Mercedez is: {m1.color}")
+
+m2 = MercedezBenz("blue")
+
+print(m2.auto_drive())
